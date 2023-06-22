2ba54ae1290702438cb8fbb94706c8b2117d119c
Vaibhav Khurana <vaibhav.khurana@hotmail.com>
2023-06-22 17:25:12 +0530

    Add functionality to consider node resources for considering better nodes

--- internal/controller/finder.go
+++ internal/controller/finder.go
@@ -1,16 +1,19 @@
 package controller
 
 import (
+	"github.com/decayofmind/kube-better-node/internal/k8s"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
 	helpers "k8s.io/component-helpers/scheduling/corev1"
 	"k8s.io/component-helpers/scheduling/corev1/nodeaffinity"
 	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/noderesources"
 )
 
-func FindBetterNode(pod *v1.Pod, curScore int, tolerance int, nodes []*v1.Node) (*v1.Node, int, error) {
+func FindBetterNode(client kubernetes.Interface, pod *v1.Pod, curScore int, tolerance int, nodes []*v1.Node) (*v1.Node, int, error) {
 	for _, node := range nodes {
-
 		// Skip nodes that do not match the Pod's NodeSelector
 		if len(pod.Spec.NodeSelector) > 0 {
 			nodeSelector := labels.SelectorFromSet(pod.Spec.NodeSelector)
@@ -32,6 +35,25 @@ func FindBetterNode(pod *v1.Pod, curScore int, tolerance int, nodes []*v1.Node) 
 				klog.V(4).InfoS("Skipping node that does not match required nodeAffinity", "node", klog.KObj(node))
 				continue
 			}
+		}
+
+		// Skip nodes that doesn't have the required resources
+		podsOnNode, err := k8s.ListPodsOnNode(client, node.ObjectMeta.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		podsWithoutLowPriority := make([]*v1.Pod, 0)
+		for _, podNode := range podsOnNode {
+			if *podNode.Spec.Priority >= *pod.Spec.Priority {
+				podsWithoutLowPriority = append(podsWithoutLowPriority, podNode)
+			}
+		}
+		nodeInfo := framework.NewNodeInfo(podsWithoutLowPriority...)
+		nodeInfo.SetNode(node)
+		insufficientResources := noderesources.Fits(pod, nodeInfo)
+		if len(insufficientResources) != 0 {
+			klog.V(4).InfoS("Skipping node that does not match required resources", "node", klog.KObj(node))
+			continue
 		}
 
 		terms, err := nodeaffinity.NewPreferredSchedulingTerms(nodeAffinity.PreferredDuringSchedulingIgnoredDuringExecution)
